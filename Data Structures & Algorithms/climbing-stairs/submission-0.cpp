cf199d11a3c86b5555606e63e6653d798c149860
Jaymil Parikh <119978905+JaymilP10@users.noreply.github.com>
2026-06-29 08:44:35 -0500

    Add: climbing-stairs - submission-0

---
+++ Data Structures & Algorithms/climbing-stairs/submission-0.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        vector<int> cache;
+        cache.push_back(1);
+        cache.push_back(1);
+        cache.push_back(2);
+        int x = 1;
+        int y = 1;
+        int z = 1;
+        if (n >= 3) {
+            for (int i = 3; i <= n; i++) {
+                x = cache[i - 1] + cache[i - 2];
+                cache.push_back(x);
+            }
+        }
+
+        return cache[n];
+
+        
+    }
+};
